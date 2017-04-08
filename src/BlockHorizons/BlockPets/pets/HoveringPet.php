ae389ad32ee44c9dd069f409b0f2b0b4dd14ca7b
Sandertv <st.ten.veldhuis@gmail.com>
2017-04-08 22:33:21 +0200

    Update HoveringPet.php

--- src/BlockHorizons/BlockPets/pets/HoveringPet.php
+++ src/BlockHorizons/BlockPets/pets/HoveringPet.php
@@ -58,26 +58,20 @@ abstract class HoveringPet extends BasePet {
 
 	public function doRidingMovement() {
 		$rider = $this->getPetOwner();
-
 		$this->pitch = $rider->pitch;
 		$this->yaw = $rider->yaw;
 		if($this->getNetworkId() === 53) {
 			$this->yaw += 180;
 		}
-
 		$x = $rider->getDirectionVector()->x;
 		$y = $rider->getDirectionVector()->y;
 		$z = $rider->getDirectionVector()->z;
-
-		$this->motionX = $this->getSpeed() * 0.3 * ($x / (abs($x) + abs($z)));
-		$this->motionZ = $this->getSpeed() * 0.3 * ($z / (abs($x) + abs($z)));
-
-		$this->motionY = 0.05;
-		if($y > 0.2 || $y < -0.2) {
-			$this->motionY = $this->getSpeed() * 0.15 * ($y / abs($y));
-			$this->motionY -= $this->motionY * $this->distance(new Vector3($this->x, $this->level->getHighestBlockAt($this->x, $this->z), $this->z)) / 5.2;
+		$this->motionX = $this->getSpeed() * 0.25 * ($x / (abs($x) + abs($z)));
+		$this->motionZ = $this->getSpeed() * 0.25 * ($z / (abs($x) + abs($z)));
+		$this->motionY = 0;
+		if($y !== 0 && abs($y) >= 0.4 && $this->distance(new Vector3($this->x, $this->level->getHighestBlockAt($this->x, $this->z), $this->z)) <= 8) {
+			$this->motionY = $this->getSpeed() * 0.10 * ($y / abs($y));
 		}
-
 		$this->move($this->motionX, $this->motionY, $this->motionZ);
 		$this->updateMovement();
 	}
