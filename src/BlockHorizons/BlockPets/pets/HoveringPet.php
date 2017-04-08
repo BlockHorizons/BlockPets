efc2bd11b9cc609bfed571114fd50eb28b7d9079
Sandertv <st.ten.veldhuis@gmail.com>
2017-04-08 08:20:37 +0200

    Changed hovering pet AI to something more efficient.

--- src/BlockHorizons/BlockPets/pets/HoveringPet.php
+++ src/BlockHorizons/BlockPets/pets/HoveringPet.php
@@ -2,9 +2,6 @@
 
 namespace BlockHorizons\BlockPets\pets;
 
-use pocketmine\block\Block;
-use pocketmine\math\Vector3;
-
 abstract class HoveringPet extends BasePet {
 
 	public function onUpdate($currentTick) {
@@ -17,38 +14,24 @@ abstract class HoveringPet extends BasePet {
 			$this->teleport($petOwner);
 			$this->spawnToAll();
 		}
-		if(!$this->isOnGround()) {
-			if($this->motionY > -$this->gravity * 4) {
-				$this->motionY = -$this->gravity * 4;
-			} else {
-				$this->motionY -= $this->gravity;
-			}
-			$this->move($this->motionX, $this->motionY, $this->motionZ);
-		}
-
-		if($this->isCollidedVertically) {
-			$this->motionY = $this->gravity * 4;
-		}
-		$this->move($this->motionX, $this->motionY, $this->motionZ);
-
 		$x = $petOwner->x - $this->x;
+		$y = $petOwner->y + 1.5 - $this->y;
 		$z = $petOwner->z - $this->z;
-		if($x * $x + $z * $z < 5) {
+
+		if($x * $x + $z * $z < 8) {
 			$this->motionX = 0;
 			$this->motionZ = 0;
 		} else {
 			$this->motionX = $this->getSpeed() * 0.15 * ($x / (abs($x) + abs($z)));
 			$this->motionZ = $this->getSpeed() * 0.15 * ($z / (abs($x) + abs($z)));
 		}
+		$this->motionY = $this->getSpeed() * 0.15 * ($y / (abs($y) + abs($y)));
+
 		$this->yaw = rad2deg(atan2(-$x, $z));
-		if($this->getNetworkId() === 53) {
-			$this->yaw += 180;
-		}
 		$this->pitch = rad2deg(atan($petOwner->y - $this->y));
 		$this->move($this->motionX, $this->motionY, $this->motionZ);
 
 		$this->updateMovement();
-
 		parent::onUpdate($currentTick);
 		return true;
 	}
