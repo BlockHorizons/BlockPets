c8dc48601fe85f922e686a32871fc6cdc7819e99
Sandertv <st.ten.veldhuis@gmail.com>
2017-04-14 15:24:36 +0200

    Potential fix of jumping. This might be a bit less reliable however.

--- src/BlockHorizons/BlockPets/pets/WalkingPet.php
+++ src/BlockHorizons/BlockPets/pets/WalkingPet.php
@@ -8,7 +8,7 @@ use pocketmine\math\Vector3;
 
 abstract class WalkingPet extends BasePet {
 
-	protected $jumpTicks;
+	protected $jumpTicks = 0;
 
 	public function onUpdate($currentTick) {
 		$petOwner = $this->getPetOwner();
@@ -58,19 +58,22 @@ abstract class WalkingPet extends BasePet {
 	public function doRidingMovement($currentTick) {
 		$rider = $this->getPetOwner();
 
-		$this->pitch = 270;
+		$this->pitch = $rider->pitch;
 		$this->yaw = $rider->yaw;
-		$this->motionY = 0;
 		if(!$this->isOnGround()) {
 			if($this->motionY > -$this->gravity * 4) {
 				$this->motionY = -$this->gravity * 4;
 			} else {
 				$this->motionY -= $this->gravity;
 			}
-		} elseif($this->isCollidedHorizontally) {
+		} elseif($this->isCollidedHorizontally && $this->jumpTicks === 0) {
 			$this->jump();
 		} else {
 			$this->motionY -= $this->gravity;
+		}
+
+		if($this->jumpTicks === 0) {
+			$this->motionY = -$this->gravity * 4;
 		}
 
 		$x = $rider->getDirectionVector()->x;
@@ -86,20 +89,7 @@ abstract class WalkingPet extends BasePet {
 	}
 
 	protected function jump() {
-		$posAhead = $this->getTargetBlock(3);
-		if($this->level->getBlock($this->getPosition())->isSolid()) {
-			$posAhead = $this->getPosition();
-		}
-		$blockAhead = $this->getLevel()->getBlock(new Vector3($posAhead->x, $posAhead->y - 0.3, $posAhead->z));
-		if($blockAhead->isSolid()) {
-			$this->motionY = $this->gravity * 8;
-			$this->move($this->motionX, $this->motionY, $this->motionZ);
-		} elseif($blockAhead instanceof Slab || $blockAhead instanceof Stair) {
-			$this->motionY = $this->gravity * 4;
-			$this->move($this->motionX, $this->motionY, $this->motionZ);
-		} else {
-			return;
-		}
-		$this->jumpTicks = 5;
+		$this->motionY = $this->gravity * 8;
+		$this->jumpTicks = 4;
 	}
 }
