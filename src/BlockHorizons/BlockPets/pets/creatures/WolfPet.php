f21aa028bfe90f6c169d886962521b875d23c125
Aaron Salyer <aaron_salyer@yahoo.com>
2018-02-11 06:08:53 -0500

    Fix Wolf Collar Colors:

--- src/BlockHorizons/BlockPets/pets/creatures/WolfPet.php
+++ src/BlockHorizons/BlockPets/pets/creatures/WolfPet.php
@@ -6,6 +6,7 @@ namespace BlockHorizons\BlockPets\pets\creatures;
 
 use BlockHorizons\BlockPets\pets\SmallCreature;
 use BlockHorizons\BlockPets\pets\WalkingPet;
+use pocketmine\Player;
 
 class WolfPet extends WalkingPet implements SmallCreature {
 
@@ -16,7 +17,10 @@ class WolfPet extends WalkingPet implements SmallCreature {
 	public $height = 0.9;
 
 	public function generateCustomPetData(): void {
-		$randomColour = random_int(0, 15);
-		$this->propertyManager->setPropertyValue(self::DATA_COLOUR, self::DATA_TYPE_BYTE, $randomColour);
-	}
+
+			$randomColour = random_int(0, 15);
+			$eid = 123456789123456789;
+			$this->propertyManager->setPropertyValue(self::DATA_OWNER_EID, self::DATA_TYPE_LONG, $eid);
+			$this->propertyManager->setPropertyValue(self::DATA_COLOUR, self::DATA_TYPE_BYTE, $randomColour);
+		}
 }
