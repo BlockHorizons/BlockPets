31151809e2520cd8432fbbef650abefcadfbf0b4
DenielW <denielmirzoyan@gmail.com>
2019-12-07 21:40:31 -0800

    Temporary fix for the spawn of non-pet entities
    The problem with the default return value being null was that when the default returnable is null and the Tag that you're trying to get does not exist in the NBT, a RuntimeException is thrown, accordingly a crash happens.

--- src/BlockHorizons/BlockPets/pets/BasePet.php
+++ src/BlockHorizons/BlockPets/pets/BasePet.php
@@ -107,8 +107,8 @@ abstract class BasePet extends Creature implements Rideable {
 	private $maxSize = 10.0;
 
 	public function __construct(Level $level, CompoundTag $nbt) {
-		$this->petOwner = $level->getServer()->getPlayerExact($nbt->getString("petOwner"));
-		if($this->petOwner === null) {
+		$this->petOwner = $level->getServer()->getPlayerExact($nbt->getString("petOwner", false));
+		if($this->petOwner === false) {
 			$this->close();
 			return;
 		}
