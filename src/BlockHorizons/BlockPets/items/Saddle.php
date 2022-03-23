42b982fd9dac28d47ee5eb58be9ac18cbac1ed56
Ifera <iferareaves@gmail.com>
2022-03-24 01:28:07 +0500

    updated saddle

--- src/BlockHorizons/BlockPets/items/Saddle.php
+++ src/BlockHorizons/BlockPets/items/Saddle.php
@@ -1,15 +1,15 @@
 <?php
-
 declare(strict_types = 1);
 
 namespace BlockHorizons\BlockPets\items;
 
 use pocketmine\item\Item;
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemIds;
 
 class Saddle extends Item {
 
 	public function __construct(int $meta = 0) {
-		parent::__construct(self::SADDLE, $meta, "Saddle");
+		parent::__construct(new ItemIdentifier(ItemIds::SADDLE, $meta), "Saddle");
 	}
-
 }
