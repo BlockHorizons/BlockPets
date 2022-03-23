7612b0877538e7867174e73fb30725b41e00af94
Ifera <iferareaves@gmail.com>
2022-03-24 01:35:57 +0500

    updates to configurables

--- src/BlockHorizons/BlockPets/configurable/BlockPetsConfig.php
+++ src/BlockHorizons/BlockPets/configurable/BlockPetsConfig.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types = 1);
 
 namespace BlockHorizons\BlockPets\configurable;
@@ -8,12 +7,9 @@ use BlockHorizons\BlockPets\Loader;
 
 class BlockPetsConfig {
 
-	private $loader;
-	private $settings;
+	private array $settings;
 
-	public function __construct(Loader $loader) {
-		$this->loader = $loader;
-
+	public function __construct(private Loader $loader) {
 		$loader->saveDefaultConfig();
 		$this->collectPreferences();
 	}
@@ -23,142 +19,82 @@ class BlockPetsConfig {
 		$this->settings = $data;
 	}
 
-	/**
-	 * @return Loader
-	 */
 	public function getLoader(): Loader {
 		return $this->loader;
 	}
 
-	/**
-	 * @return bool
-	 */
 	public function doHardReset(): bool {
 		return (bool) ($this->settings["Hard-Reset"] ?? false);
 	}
 
-	/**
-	 * @return bool
-	 */
 	public function arePetsInvulnerable(): bool {
 		return (bool) ($this->settings["Invulnerable-Pets"] ?? false);
 	}
 
-	/**
-	 * @return bool
-	 */
 	public function petsDoAttack(): bool {
 		return (bool) ($this->settings["Attacking-Pets"] ?? true);
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getBasePetHealth(): int {
 		return (int) ($this->settings["Pet-Base-Health"] ?? 20);
 	}
 
-	/**
-	 * @return float
-	 */
 	public function getPetHealthPerLevel(): float {
 		return (float) ($this->settings["Pet-Per-Level-Health"] ?? 0.5);
 	}
 
-	/**
-	 * @return float
-	 */
 	public function getPetSizePerLevel(): float {
 		return (float) ($this->settings["Pet-Per-Level-Size"] ?? 0.03);
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getBasePetDamage(): int {
 		return (int) ($this->settings["Pet-Base-Damage"] ?? 4);
 	}
 
-	/**
-	 * @return float
-	 */
 	public function getPetDamagePerLevel(): float {
 		return (float) ($this->settings["Pet-Per-Level-Damage"] ?? 0.1);
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getRespawnTime(): int {
 		return (int) ($this->settings["Pet-Respawn-Time"] ?? 10);
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getPlayerExperiencePoints(): int {
 		return (int) ($this->settings["Experience-Points-Per-Player-Kill"] ?? 15);
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getEntityExperiencePoints(): int {
 		return (int) ($this->settings["Experience-Points-Per-Entity-Kill"] ?? 10);
 	}
 
-	/**
-	 * @return array
-	 */
 	public function getMySQLInfo(): array {
 		return (array) ($this->settings["MySQL-Info"] ?? []);
 	}
 
-	/**
-	 * @return bool
-	 */
 	public function storeToDatabase(): bool {
 		return (bool) ($this->settings["Store-To-Database"] ?? true);
 	}
 
-	/**
-	 * @return string
-	 */
 	public function getDatabase(): string {
 		return (string) ($this->settings["Database"] ?? "SQLite");
 	}
 
-	/**
-	 * @return string
-	 */
 	public function getLanguage(): string {
 		return (string) ($this->settings["Language"] ?? "en");
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getMaxPets(): int {
 		return (int) ($this->settings["Pet-Limit"] ?? 3);
 	}
 
-	/**
-	 * @return bool
-	 */
 	public function arePetsInvulnerableIfOwnerIs(): bool {
 		return (bool) ($this->settings["Invulnerable-If-Owner-Is"] ?? true);
 	}
 
-	/**
-	 * @return bool
-	 */
 	public function giveExperienceWhenFed(): bool {
 		return (bool) ($this->settings["Give-Experience-For-Feeding"] ?? true);
 	}
 
-	/**
-	 * @return bool
-	 */
 	public function shouldStalkPetOwner(): bool {
 		return (bool) ($this->settings["Stalk-Owner-Blindly"] ?? false);
 	}
