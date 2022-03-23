62724059e2590bd5c42b92e09c6c103722ee891f
Ifera <iferareaves@gmail.com>
2022-03-24 01:37:23 +0500

    update BaseCommand

--- src/BlockHorizons/BlockPets/commands/BaseCommand.php
+++ src/BlockHorizons/BlockPets/commands/BaseCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types = 1);
 
 namespace BlockHorizons\BlockPets\commands;
@@ -7,27 +6,19 @@ namespace BlockHorizons\BlockPets\commands;
 use BlockHorizons\BlockPets\Loader;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\command\PluginIdentifiableCommand;
-use pocketmine\Player;
 use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned;
 use pocketmine\utils\TextFormat as TF;
 
-abstract class BaseCommand extends Command implements PluginIdentifiableCommand {
+abstract class BaseCommand extends Command implements PluginOwned {
 
-	/** @var Loader */
-	protected $loader;
-
-	public function __construct(Loader $loader, string $name, string $description = "", string $usageMessage = "", array $aliases = []) {
+	public function __construct(protected Loader $loader, string $name, string $description = "", string $usageMessage = "", array $aliases = []) {
 		if($usageMessage !== "") {
 			$usageMessage = TF::RED . "Usage: " . $usageMessage;
 		}
 		parent::__construct($name, $description, $usageMessage, $aliases);
-		$this->loader = $loader;
 	}
 
-	/**
-	 * @param CommandSender $sender
-	 */
 	public function sendConsoleError(CommandSender $sender, bool $noConsoleUse = false): void {
 		if($noConsoleUse) {
 			$this->sendWarning($sender, $this->getLoader()->translate("commands.errors.no-console-use"));
@@ -36,41 +27,24 @@ abstract class BaseCommand extends Command implements PluginIdentifiableCommand 
 		$this->sendWarning($sender, $this->getLoader()->translate("commands.errors.console-use"));
 	}
 
-	/**
-	 * @param CommandSender $sender
-	 * @param string        $text
-	 */
 	public function sendWarning(CommandSender $sender, string $text): void {
 		$sender->sendMessage(TF::RED . $this->getLoader()->translate("prefix.warning") . " " . $text);
 	}
 
-	/**
-	 * @param CommandSender $sender
-	 */
 	public function sendPermissionMessage(CommandSender $sender): void {
 		$this->sendWarning($sender, $this->getLoader()->translate("commands." . $this->getName() . ".no-permission") ?? $this->getLoader()->translate("commands.no-permission"));
 	}
 
-	/**
-	 * @return Loader
-	 */
 	public function getLoader(): Loader {
 		return $this->loader;
 	}
 
-	/**
-	 * @return Loader
-	 */
 	public function getPlugin(): Plugin {
 		return $this->loader;
 	}
 
 	/**
-	 * @param CommandSender $sender
-	 * @param string        $commandLabel
-	 * @param string[]      $args
-	 *
-	 * @return mixed
+	 * @param string[] $args
 	 */
 	public final function execute(CommandSender $sender, string $commandLabel, array $args): void {
 		if(!$this->testPermissionSilent($sender)) {
