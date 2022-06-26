edf0f54fed3dd4dba7465c87125254b20708e26b
David <noobgamery567@gmail.com>
2022-06-26 07:31:16 +0700

    I have completed the reviews, please review again!

---
+++ src/BlockHorizons/BlockPets/pets/creatures/AllayPet.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace BlockHorizons\BlockPets\pets\creatures;
+
+use BlockHorizons\BlockPets\pets\HoveringPet;
+use BlockHorizons\BlockPets\pets\SmallCreature;
+
+class AllayPet extends HoveringPet implements SmallCreature {
+
+	const NETWORK_NAME = "ALLAY_PET";
+	const NETWORK_ORIG_ID = self::ALLAY;
+
+	protected string $name = "Allay Pet";
+
+	protected float $width = 0.6;
+	protected float $height = 0.6;
+
+}
---
+++ src/BlockHorizons/BlockPets/pets/creatures/FoxPet.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace BlockHorizons\BlockPets\pets\creatures;
+
+use BlockHorizons\BlockPets\pets\SmallCreature;
+use BlockHorizons\BlockPets\pets\WalkingPet;
+
+class FoxPet extends WalkingPet implements SmallCreature {
+
+	const NETWORK_NAME = "FOX_PET";
+	const NETWORK_ORIG_ID = self::FOX;
+
+	protected string $name = "Fox Pet";
+
+	protected float $width = 0.6;
+	protected float $height = 0.7;
+
+}
